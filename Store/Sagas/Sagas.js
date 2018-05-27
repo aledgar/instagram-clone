fad29e6be3976bc05f81eb00052f94918795db13
aledgar <aledgar.2710@gmail.com>
2018-05-27 15:25:06 -0500

    Adding saga

--- Store/Sagas/Sagas.js
+++ Store/Sagas/Sagas.js
@@ -1,3 +1,26 @@
+import { takeEvery, call } from 'redux-saga/effects';
+import {autentication} from '../Servicios/Firebase';
+
+registroFireBase = (values) =>{
+    autentication.createUserWithEmailAndPassword(values.correo,values.password)
+        .then((success)=>{
+            alert('Se registro el usuario.');
+            console.log(success); 
+        })
+        .catch((error)=>{
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(error);
+            console.log(error) 
+        })
+}
+
+function* generadoraRegistro (values){
+    yield call(registroFireBase, values.datos);
+    console.log(values);
+}
+
 export default function* funcionPrimaria(){
-    console.log('Entra a la funcion primaria')
+    yield takeEvery('REGISTRY',generadoraRegistro);
+    console.log('Entra a la funcion primaria');
 }
